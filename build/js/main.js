f04ba0536303a5df34d3087904ae8a3f25ec3dc5
Александр Слав <Aleksslav@gmail.com>
2021-03-15 11:45:09 +0300

    Правки 15,03,2021

--- build/js/main.js
+++ build/js/main.js
@@ -1,74 +1,70 @@
 'use strict';
 
 (function () {
-  var KEYCODE = {
+  const KEYCODE = {
     esc: 27
   };
-  var link = document.querySelector('.main-nav__button');
-  var popup = document.querySelector('.modal-overlay');
-  var close = popup.querySelector('.modal-overlay__close');
-  var form = popup.querySelector('.modal-overlay__form');
-  var modalName = popup.querySelector('#modal-name');
-  var modalPhone = popup.querySelector('#modal-phone');
-  var modalLetter = popup.querySelector('#modal-letter');
-  var isStorageSupport = true;
-  var storage = {};
+  const link = document.querySelector(`.main-nav__button`);
+  const popup = document.querySelector(`.modal-overlay`);
+  const close = popup.querySelector(`.modal-overlay__close`);
+  const form = popup.querySelector(`.modal-overlay__form`);
+  const modalName = popup.querySelector(`#modal-name`);
+  const modalPhone = popup.querySelector(`#modal-phone`);
+  const modalLetter = popup.querySelector(`#modal-letter`);
+  const isStorageSupport = true;
+  const storage = {};
 
-  var openPopup = function () {
-    popup.classList.add('modal-overlay--opened');
-    document.body.classList.add('modal-overlay__no-scroll');
+  const openPopup = function () {
+    popup.classList.add(`modal-overlay--opened`);
+    document.body.classList.add(`modal-overlay__no-scroll`);
   };
 
-  var closePopup = function () {
-    popup.classList.remove('modal-overlay--opened');
-    document.body.classList.remove('modal-overlay__no-scroll');
+  const closePopup = function () {
+    popup.classList.remove(`modal-overlay--opened`);
+    document.body.classList.remove(`modal-overlay__no-scroll`);
   };
 
   try {
-    storage.name = localStorage.getItem('name');
-    storage.phone = localStorage.getItem('phone');
-    storage.letter = localStorage.getItem('letter');
+    storage.name = localStorage.getItem(`name`);
+    storage.phone = localStorage.getItem(`phone`);
+    storage.letter = localStorage.getItem(`letter`);
   } catch (err) {
     isStorageSupport = false;
   }
 
-  link.addEventListener('click', function (evt) {
+  link.addEventListener(`click`, function (evt) {
     evt.preventDefault();
     openPopup();
 
-    if (storage.name) {
-      modalName.value = storage.name;
-      modalPhone.value = storage.phone;
-      modalLetter.value = storage.letter;
-      modalLetter.focus();
-    } else {
-      modalName.focus();
-    }
+    modalName.value = storage.name;
+    modalPhone.value = storage.phone;
+    modalLetter.value = storage.letter;
+    modalName.focus();
   });
 
-  close.addEventListener('click', function (evt) {
+  close.addEventListener(`click`, function (evt) {
     evt.preventDefault();
     closePopup();
   });
 
-  form.addEventListener('submit', function () {
+  form.addEventListener(`submit`, function () {
     if (isStorageSupport) {
-      localStorage.setItem('name', modalName.value);
-      localStorage.setItem('phone', modalPhone.value);
-      localStorage.setItem('letter', modalLetter.value);
+      localStorage.setItem(`name`, modalName.value);
+      localStorage.setItem(`phone`, modalPhone.value);
+      localStorage.setItem(`letter`, modalLetter.value);
     }
   });
 
-  window.addEventListener('keydown', function (evt) {
+  window.addEventListener(`keydown`, function (evt) {
     if (evt.keyCode === KEYCODE.esc) {
       evt.preventDefault();
-      if (popup.classList.contains('modal-overlay--opened')) {
+      if (popup.classList.contains(`modal-overlay--opened`)) {
         closePopup();
       }
     }
   });
 
-  popup.addEventListener('click', function (evt) {
+  popup.addEventListener(`click`, function (evt) {
     if (evt.target === popup) {
       closePopup();
     }
@@ -76,23 +72,40 @@
 })();
 
 (function () {
-  var accordionItems = document.querySelectorAll('.accordion');
-  var accordionPanes = document.querySelectorAll('.accordion__pane');
+  const anchors = document.querySelectorAll(`a[href*="#"]`);
 
-  var hidePane = function (button, pane) {
-    button.classList.add('accordion__toggle--inactive');
-    pane.classList.add('accordion__pane--hidden');
+  for (let anchor of anchors) {
+    anchor.addEventListener(`click`, function (e) {
+      e.preventDefault();
+
+      const blockID = anchor.getAttribute(`href`).substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: `smooth`,
+        block: `start`,
+      });
+    });
+  }
+})();
+
+(function () {
+  const accordionItems = document.querySelectorAll(`.accordion`);
+  const accordionPanes = document.querySelectorAll(`.accordion__pane`);
+
+  const hidePane = function (button, pane) {
+    button.classList.add(`accordion__toggle--inactive`);
+    pane.classList.add(`accordion__pane--hidden`);
   };
 
-  var showPane = function (button, pane) {
-    button.classList.remove('accordion__toggle--inactive');
-    pane.classList.remove('accordion__pane--hidden');
+  const showPane = function (button, pane) {
+    button.classList.remove(`accordion__toggle--inactive`);
+    pane.classList.remove(`accordion__pane--hidden`);
   };
 
-  var toggleAccordion = function (evt) {
-    Array.prototype.forEach.call(accordionPanes, function (accordionPane) {
-      var button = accordionPane.closest('.accordion').querySelector('.accordion__toggle');
-      if (button === evt.target && !button.classList.contains('accordion__toggle--inactive') || button !== evt.target) {
+  const toggleAccordion = function (evt) {
+    accordionPanes.forEach(function (accordionPane) {
+      const button = accordionPane.closest(`.accordion`).querySelector(`.accordion__toggle`);
+      if (button === evt.target && !button.classList.contains(`accordion__toggle--inactive`) || button !== evt.target) {
         hidePane(button, accordionPane);
       } else if (button === evt.target) {
         showPane(button, accordionPane);
@@ -100,23 +113,38 @@
     });
   };
 
-  Array.prototype.forEach.call(accordionItems, function (accordion) {
-    var accordionToggleButton = accordion.querySelector('.accordion__toggle');
-    var accordionPane = accordion.querySelector('.accordion__pane');
+  accordionItems.forEach(function (accordion) {
+    const accordionToggleButton = accordion.querySelector(`.accordion__toggle`);
+    accordionToggleButton.classList.remove(`accordion__toggle--nojs`);
+    const accordionPane = accordion.querySelector(`.accordion__pane`);
     hidePane(accordionToggleButton, accordionPane);
-    accordionToggleButton.addEventListener('click', toggleAccordion);
+    accordionToggleButton.addEventListener(`click`, toggleAccordion);
+  });
+
+  accordionItems.forEach((acc)=>{
+    acc.addEventListener(`click`, (evt)=>{
+
+      document.querySelectorAll(`.accordion__pane`).forEach((item)=>{
+        item.classList.add(`accordion__pane--hidden`);
+      });
+      document.querySelectorAll(`.accordion__toggle`).forEach((item)=>{
+        item.classList.add(`accordion__toggle--inactive`);
+      });
+      evt.currentTarget.querySelector(`.accordion__pane`).classList.remove(`accordion__pane--hidden`);
+      evt.currentTarget.querySelector(`.accordion__toggle`).classList.remove(`accordion__toggle--inactive`);
+    });
   });
 })();
 
 /*eslint-disable*/
 
 (function () {
-  var maskedInputs = document.querySelectorAll('input[data-inputmask]');
+  const maskedInputs = document.querySelectorAll(`input[data-inputmask]`);
 
-  var applyMask = function () {
-    Array.prototype.forEach.call(maskedInputs, function (input) {
-      var maskOption = {
-        mask: input.getAttribute('data-inputmask')
+  const applyMask = function () {
+    maskedInputs.forEach(function (input) {
+      const maskOption = {
+        mask: input.getAttribute(`data-inputmask`)
       };
       IMask(input, maskOption);
     });
